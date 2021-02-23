0a545118f39d87ef7bd464df7828a01513c84e78
gcaptn <captain.g.1337@gmail.com>
2021-02-23 22:22:53 +0700

    fix

--- calmtype/free/script.js
+++ calmtype/free/script.js
@@ -1,11 +1,10 @@
 var editorlua = CodeMirror.fromTextArea(document.getElementById("main-typer"), {
-    lineNumbers: true,
-    mode: "lua",
+    lineNumbers: false,
     indentUnit: 4,
     theme: "rdark",
     viewportMargin: Infinity,
     lineWrapping: true
 })
 
-var placeholder = 'local this_is = "the start of a great journey."'
+var placeholder = 'This is the start of a great journey.'
 editorlua.setValue(placeholder)
--- calmtype/js/script.js
+++ calmtype/js/script.js
@@ -1,10 +1,11 @@
 var editorjs = CodeMirror.fromTextArea(document.getElementById("main-typer"), {
-    lineNumbers: false,
+    lineNumbers: true,
+    mode: "javascript",
     indentUnit: 4,
     theme: "rdark",
     viewportMargin: Infinity,
     lineWrapping: true
 })
 
-var placeholder = 'This is the start of a great journey.'
+var placeholder = 'var this_is = "the start of a great journey.";'
 editorjs.setValue(placeholder)
